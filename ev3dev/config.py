29222b3949bfe071627f99ba8c3649ff62126d0a
richard <richardkurle@gmail.com>
2017-09-11 17:28:43 +0000

    IP changes

--- ev3dev/config.py
+++ ev3dev/config.py
@@ -1,6 +1,6 @@
 # NOTE: This config should be the same as on the Jetson!
 
-BROKER_IP = "10.250.144.203"  # This is the IP address of your Jetson, connected to EV3 (check with ifconfig)
+BROKER_IP = "10.180.226.162"  # This is the IP address of your Jetson, connected to EV3 (check with ifconfig)
 BROKER_PORT = 1883
 
 ACTUATORS = 'actuators'
