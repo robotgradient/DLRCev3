1c09b12ae2c1feb221791c203436976af112f0ab
Ilia Kurenkov <ilia.kurenkov@gmail.com>
2017-09-12 11:55:57 +0200

    final take

--- ev3dev/gitpuller.sh
+++ ev3dev/gitpuller.sh
@@ -1,1 +1,5 @@
-while sleep 1; do git pull origin master; done
+#!/usr/bin/env bash
+# Here's how you run this:
+# screen -DS puller ev3dev/gitpuller.sh
+
+while sleep 10; do git pull origin master; done
