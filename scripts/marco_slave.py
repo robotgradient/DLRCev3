acb86190241e3deaa6629aabc9836ca7d3e1e0a5
Marco Ojer <marco.ojer@gmail.com>
2017-09-14 11:55:09 +0200

    my slave

---
+++ scripts/marco_slave.py
@@ -0,0 +1,9 @@
+"""Example of a slave script.
+
+The only thing you should need to change is the IP of the 'master' computer.
+You shouldn't need to modify anything in the slave logic.
+"""
+from ev3control import run_slave
+
+run_slave("10.180.226.193")
+
