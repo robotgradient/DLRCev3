3e5956884305168ebb765475372cef669b7432b9
Ilia Kurenkov <ilia.kurenkov@gmail.com>
2017-09-21 09:45:01 +0200

    First draft of rpc-based code

--- ev3control/setup.py
+++ ev3control/setup.py
@@ -5,6 +5,7 @@ setup(
     name='ev3control',
     packages=find_packages(),
     install_requires=[
-        'paho-mqtt>=1.3',
+        'paho-mqtt==1.3',
+        "rpyc==3.4.4",
         "python-ev3dev==1.0.0"
     ],)
