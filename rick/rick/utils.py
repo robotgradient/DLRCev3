710768f1b62837ce125660cafd1b1645662a4a60
Marin Vlastelica <marin.vlastelicap@gmail.com>
2017-09-26 17:16:30 +0200

    Added some utils

--- rick/rick/utils.py
+++ rick/rick/utils.py
@@ -1,5 +1,6 @@
 import numpy as np
-
+import cv2
+from .utils import *
 
 def bbox_center(xmin, ymin, xmax, ymax):
     return np.asarray([(xmin + xmax) / 2., (ymin + ymax) / 2.])
@@ -27,15 +28,42 @@ class DetectionTrackingCoordinator(object):
         self.detector = detector
 
     @property
-    def bbox(self):
+    def first_coords(self, frame, img_res):
         tracker_bbox = self.tracker.bbox
         detector_bbox = self.detector.bbox
 
-        if not detector_bbox is None and not tracker_bbox is None:
-            # Check if they are overlapping, if yes return the detector_box and update tracker, if not, return tracker
-            pass
-        if detector_bbox is None and not tracker_bbox is None:
-            pass
-        elif detector_bbox is not None:
-            pass
+        img = frame
+        kernel = np.ones((5,5),np.float32)/25
+        img = cv2.filter2D(img,-1,kernel)
+        res = robot.object_detector.detect_with_threshold(img,threshold=0.7, return_closest=True)
 
+        box, score = res[0] 
+        img_res = np.asarray(img_res)
+        coords = bbox_center(box[1], box[0], box[3], box[2]) * img_res
+        coords = coords.astype(np.int16)
+        atol = 10 + coords[1]/480 * 50
+        print("Coords: ", coords)
+        cv2.rectangle(frame, (int(box[1]*img_res[0]), int(box[0]*img_res[1])), 
+                (int(box[3]*img_res[0]), int(box[2]*img_res[1])), (0, 255, 0), thickness=3)    
+        cv2.putText(frame, str(score) + "%", (int(box[1]*img_res[0]),
+         int(box[0]*img_res[1])), cv2.FONT_HERSHEY_SIMPLEX, 0.6, color=(255,0,0))
+        cv2.circle(frame,tuple(coords),2,(255,255,255),thickness=2)
+
+        if not detector_bbox is None and not tracker_bbox is None:
+
+            if bboxes_are_overlapping(detector_bbox, tracker_bbox):
+                tracker_bbox.init(frame, detector_bbox)
+                return detector_bbox
+            else:
+                return tracker_bbox
+
+        if detector_bbox is None and not tracker_bbox is None:
+            return tracker_bbox
+        elif detector_bbox is not None:
+            tracker_bbox.init(frame, detector_bbox)
+            return detector_bbox
+        else:
+            return None
+
+
+        return coords, frame
--- unsupervised_models/unsupervised_models/convolutional_variational_autoencoder.py
+++ unsupervised_models/unsupervised_models/convolutional_variational_autoencoder.py
@@ -188,27 +188,27 @@ vae.fit(x_train,
 
 
 
+if __name__ == '__main__':
+    # display a 2D manifold of the digits
+    n = 15  # figure with 15x15 digits
+    digit_size = 28
+    figure = np.zeros((digit_size * n, digit_size * n))
+    # linearly spaced coordinates on the unit square were transformed through the inverse CDF (ppf) of the Gaussian
+    # to produce values of the latent variables z, since the prior of the latent space is Gaussian
+    grid_x = norm.ppf(np.linspace(0.05, 0.95, n))
+    grid_y = norm.ppf(np.linspace(0.05, 0.95, n))
 
-# display a 2D manifold of the digits
-n = 15  # figure with 15x15 digits
-digit_size = 28
-figure = np.zeros((digit_size * n, digit_size * n))
-# linearly spaced coordinates on the unit square were transformed through the inverse CDF (ppf) of the Gaussian
-# to produce values of the latent variables z, since the prior of the latent space is Gaussian
-grid_x = norm.ppf(np.linspace(0.05, 0.95, n))
-grid_y = norm.ppf(np.linspace(0.05, 0.95, n))
+    batch_size = 16
 
-batch_size = 16
+    for i, yi in enumerate(grid_x):
+        for j, xi in enumerate(grid_y):
+            z_sample = np.array([[xi, yi]])
+            z_sample = np.tile(z_sample, batch_size).reshape(batch_size, 2)
+            x_decoded = vae.generator.predict(z_sample, batch_size=batch_size)
+            digit = x_decoded[0].reshape(digit_size, digit_size)
+            figure[i * digit_size: (i + 1) * digit_size,
+                   j * digit_size: (j + 1) * digit_size] = digit
 
-for i, yi in enumerate(grid_x):
-    for j, xi in enumerate(grid_y):
-        z_sample = np.array([[xi, yi]])
-        z_sample = np.tile(z_sample, batch_size).reshape(batch_size, 2)
-        x_decoded = vae.generator.predict(z_sample, batch_size=batch_size)
-        digit = x_decoded[0].reshape(digit_size, digit_size)
-        figure[i * digit_size: (i + 1) * digit_size,
-               j * digit_size: (j + 1) * digit_size] = digit
-
-plt.figure(figsize=(10, 10))
-plt.imshow(figure, cmap='Greys_r')
-plt.show()
+    plt.figure(figsize=(10, 10))
+    plt.imshow(figure, cmap='Greys_r')
+    plt.show()
