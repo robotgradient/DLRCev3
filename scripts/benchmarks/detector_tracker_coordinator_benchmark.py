034f3a14326b5b0f38e25c1f151e33a4a76724ce
Marin Vlastelica <marin.vlastelicap@gmail.com>
2017-09-27 13:19:49 +0200

    Working benchmark for tracking and detecting

--- scripts/benchmarks/detector_tracker_coordinator_benchmark.py
+++ scripts/benchmarks/detector_tracker_coordinator_benchmark.py
@@ -2,7 +2,6 @@ from rick.utils import *
 from rick.async import *
 from nn_object_detection.object_detectors import NNObjectDetector
 import cv2 
-from rick.utils import bbf, bbdn, bboxes_are_overlapping
 
 
 # Path to frozen detection graph. This is the actual model that is used for the object detection.
@@ -13,7 +12,7 @@ NUM_CLASSES = 2
 object_detector = NNObjectDetector(PATH_TO_CKPT, PATH_TO_LABELS)
 
 
-tracker = cv2.TrackerKCF_create()
+tracker = TrackerWrapper(cv2.TrackerKCF_create)
 #tracker.init(frame,bbox)
 #ok, bbox = tracker.update(frame)
 
@@ -21,64 +20,50 @@ detection_tracker = DetectionTrackingCoordinator(tracker, object_detector)
 camera = AsyncCamera(0)
 img_res = (640, 480)
 
+def plot_bbox(frame,bbox, score=0, color=(0,255,0)):
+    cv2.rectangle(frame, (box[1], box[0]), (box[3], box[2]), (0, 255, 0), thickness=3)    
+    cv2.putText(frame, str(score) + "%", (box[0], box[1]), cv2.FONT_HERSHEY_SIMPLEX, 0.6, color=(255,0,0))
+
 while True:
     ok, frame = camera.frame
 
     # Display result
-    img = frame
-    kernel = np.ones((5,5),np.float32)/25
-    img = cv2.filter2D(img,-1,kernel)
-    res = object_detector.detect_with_threshold(img,threshold=0.9, return_closest=True)
-    print("Res: ", res)
-    ok, bbox = tracker.update(frame)
-    bbox = (bbox[0], bbox[1], bbox[0] + bbox[2], bbox[1] + bbox[3])
-    ok = False
-    if len(res)==0 and ok:
-        box = bbox
-        print("Plotting tracking")
-        cv2.rectangle(frame, (int(box[1]*img_res[0]), int(box[0]*img_res[1])), 
-            (int(box[3]*img_res[0]), int(box[2]*img_res[1])), (0, 0, 255), thickness=2)
-        cv2.imshow("Tracking", frame)
-        # Exit if ESC pressed
-        k = cv2.waitKey(1) & 0xff
-        if k == 27:
+    img = apply_filter(frame)
+
+
+    res = object_detector.detect_with_threshold(img, threshold=0.95, return_closest=True)
+
+
+    ok, tbox = tracker.update(frame)
+    print(tbox)
+
+    if len(res) > 0:
+        for r in res:
+
+            box, score = r 
+
+
+            box = np.asarray((int(box[1]*img_res[0]), int(box[0]*img_res[1]), int(box[3]*img_res[0]), int(box[2]*img_res[1])))
+            if ok and not bboxes_are_overlapping(box, tbox):
+                cv2.rectangle(frame, (tbox[0], tbox[1]), 
+                    (tbox[2], tbox[3]), (255, 0, 0), thickness=3)
+                break
+
+            img_res = np.asarray(img_res)
+            cv2.rectangle(frame, (box[0], box[1]), 
+                    (box[2], box[3]), (0, 255, 0), thickness=3)    
+            cv2.putText(frame, str(score) + "%", (box[0], box[1]), cv2.FONT_HERSHEY_SIMPLEX, 0.6, color=(255,0,0))
+
+            tracker.init(frame, box)
+
             break
-        continue
-    elif len(res)==0:
-        continue
-        cv2.imshow("Tracking", frame)
-        # Exit if ESC pressed
-        k = cv2.waitKey(1) & 0xff
-        if k == 27:
-            break
-        continue
     elif ok:
-        box_net, score = res[0]
-        box_net = bbdn(bbf(box_net), img_res)
-        if not bboxes_are_overlapping(box, box_net):
-            bbox
+        cv2.rectangle(frame, (tbox[0], tbox[1]), 
+                    (tbox[2], tbox[3]), (255, 0, 0), thickness=3)
 
-    box, score = res[0]
-    box = bbdn(bbf(box), img_res)
-    print("Initializing tracker...") 
-    tracker.clear()
-    tracker = cv2.TrackerKCF_create()
-    kcf_init =  (box[0], box[1], box[2]-box[0], box[3] - box[1])
-    print(kcf_init)
-    tracker.init(frame, kcf_init)
-    print("Shape: ", res[0])
-    img_res = np.asarray(img_res)
-    coords = bbox_center(box[1], box[0], box[3], box[2]) * img_res
-
-    coords = coords.astype(np.int16)
-    atol = 10 + coords[1]/480 * 50
-    print("Coords: ", coords)
-    cv2.rectangle(frame, (box[0], box[1]), (box[2], box[3]), (0, 255, 0), thickness=3)    
-    cv2.putText(frame, str(score) + "%", (int(box[1]*img_res[0]),
-     int(box[0]*img_res[1])), cv2.FONT_HERSHEY_SIMPLEX, 0.6, color=(255,0,0))
-    cv2.circle(frame,tuple(coords),2,(255,255,255),thickness=2)
     cv2.imshow("Tracking", frame)
     # Exit if ESC pressed
     k = cv2.waitKey(1) & 0xff
-    if k == 27: break
+    if k == 27:
+        break
 
