30d46ff733bbaf76375df400a7a8b15d09714860
TheCamusean <judj.core@gmail.com>
2017-09-27 15:22:11 +0200

    mapping

--- scripts/slam/mapping.py
+++ scripts/slam/mapping.py
@@ -1,8 +1,10 @@
 import numpy as np
+import cv2
 
 
-def points2map(landmarks,pos_rob,map,P):
+def points2map(landmarks,pos_rob,map,P, delete_countdown): #MAP = (x,y, Px,Py, updt)
 
+	new_points2add = []
 	for i in range(0,len(landmarks[:,1])):
 
 			x_map = pos_rob[0] + landmarks[i,0]*np.cos(pos_rob[2]+landmarks[i,1])
@@ -13,17 +15,28 @@ def points2map(landmarks,pos_rob,map,P):
 			new = 1
 
 			map_ar = np.array(map)
-			for i in range(0, len(map[:,1])):
 
-				distance = np.power((x_map-map_ar[i,1])/map_ar[i,3],2) + np.power((y_map - map_ar[i,2])/map_ar[i,4],2)
+			if delete_countdown ==5:
+				map_ar[:,4] = np.zeros([map_ar.shape(0)])
+				delete_countdown = 0
 
-				if distance < 1 : 
-					new = 0
+			sh_dist = 100000;
+			p_already = 0
+			for j in range(0, len(map[:,1])):
+
+				distance = np.power((x_map-map_ar[j,1])/map_ar[j,3],2) + np.power((y_map - map_ar[j,2])/map_ar[j,4],2)
+
+				if sh_dist > distance: 
+					sh_dist = distance
+					p_already = j
+			if sh_dist < 1:
+				map[j,4] = 1
+
 			if new ==1:
-				map.append([x_map,y_map, P[0,0] , P[1,1] ])
+				new_points2add.append(i)
 
-
-			return map
+	delete_countdown +=1
+	return map , delete_countdown , new_points2add
 
 
 def cam2rob(landmarks, H):
@@ -85,6 +98,87 @@ def map2grid(map):
 			
 
 	return map
+
+
+
+
+
+def delete_in_map(map,robot_trajectory):
+
+	robot_trajectory_ar = np.array(robot_trajectory)
+
+	min_dist = 29
+	max_dist = 60
+	min_angle = np.arctan2(29,15)
+	max_angle = np.arctan2(29,-15)
+	map_ar = np.array(map)
+
+	eliminate_index = []
+	for i in range(0, robot_trajectory_ar.shape(0)):
+
+		for j in range(0, map_ar.shape(0)):
+
+			x = map_ar[j,0] - robot_trajectory_ar[i,0]
+			y = map_ar[j,1] - robot_trajectory_ar[i,1]
+
+			distance = np.sqrt(np.power(x,2)+np.power(y,2))
+			angle = np.arctan2(y,x) - robot_trajectory_ar(i,2)*pi/180
+
+			if distance > min_dist and distance< max_dist and angle > min_angle and angle< max_angle and map_ar[j,4] == 0 and not(j in eliminate_index):
+				eliminate_index.append([j])
+
+	del map[eliminate_index]
+
+	return map
+
+
+def add_points_in_map(landmarks,new_points2add,map, P ):
+
+	for i in range new_points2add:
+
+			x_map = pos_rob[0] + landmarks[i,0]*np.cos(pos_rob[2]+landmarks[i,1])
+
+			y_map = pos_rob[1] + landmarks[i,0]*np.sin(pos_rob[2]+landmarks[i,1])
+
+			map.append(x_map,y_map,P[0,0],P[1,1],1)
+
+	return map
+
+
+
+
+
+
+
+
+
+
+def update_map(map,landmark_rob,pos_rob,P,delete_countdown, robot_trajectory):
+
+
+	map,delete_countdown, new_points2add = points2map(landmarks_rob, pos_rob, map, P, delete_countdown)
+
+	robot_trajectory.append(pos_rob)
+
+	map = new_points2add(landmarks_rob,new_points2add,map,P)
+
+
+
+	if delete_countdown == 5:
+
+		map = delete_in_map(map, robot_trajectory)
+
+		robot_trajectory = [];
+
+
+	return map, delete_countdown,robot_trajectory
+
+
+
+
+
+
+
 
 
 
