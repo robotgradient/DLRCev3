a15f662bb6d319e7a6fdaf9e430602f94371562d
Ilia Kurenkov <ilia.kurenkov@gmail.com>
2017-09-28 16:55:52 +0200

    Updated training script to test in docker

--- scripts/train_vae.py
+++ scripts/train_vae.py
@@ -1,6 +1,8 @@
 from functools import partial
 
 import os
+import sys
+
 from itertools import cycle
 from keras.callbacks import ModelCheckpoint, TensorBoard
 
@@ -9,12 +11,10 @@ import numpy as np
 
 from duckdata.blender import blender_data_gen
 from unsupervised_models.cvae import ConvolutionalVariationalAutoencoder
-from keras import optimizers
 
 IM_SIZE = 64
 
 if __name__ == '__main__':
-
 
     latent_dim = int(sys.argv[1])
     batch_size = 16
@@ -22,39 +22,43 @@ if __name__ == '__main__':
     epochs = latent_dim * 4
 
     vae = ConvolutionalVariationalAutoencoder(
-    image_dims=(IM_SIZE, IM_SIZE, 3), batch_size=batch_size, latent_dim=latent_dim, filters=32)
-
-    #optimizer = optimizers.Adam(lr=0.01)
+        image_dims=(IM_SIZE, IM_SIZE, 3), batch_size=batch_size, latent_dim=latent_dim, filters=32)
 
     # vae.compile(optimizer='rmsprop', loss=vae.output_layers[0].losses[0])
-    vae.compile(optimizer="rmsprop", loss=None,
-    # metrics=[vae.output_layers[0].losses[0]]
+    vae.compile(
+        optimizer='rmsprop',
+        loss=None,
+        # metrics=[vae.output_layers[0].losses[0]]
     )
     print(vae.summary())
 
-    os.makedirs('/home/dlrc/Desktop/vae_models/weights/latent_dims_' + str(latent_dim), exist_ok=True)
-    checkpoint_callback = ModelCheckpoint("/home/dlrc/Desktop/vae_models/weights/latent_dims_"+str(latent_dim)+"/weights.{epoch:02d}.hdf5",
-    # monitor="custom_variational_layer_1/Mean_2:0",
-    verbose=1,
-    save_weights_only=False,
-    mode='auto',
-    period=10)
+    os.makedirs('/data/weights/latent_dims_' + str(latent_dim), exist_ok=True)
+    checkpoint_callback = ModelCheckpoint(
+        "/home/dlrc/Desktop/vae_models/weights/latent_dims_" + str(latent_dim) +
+        "/weights.{epoch:02d}.hdf5",
+        # monitor="custom_variational_layer_1/Mean_2:0",
+        verbose=1,
+        save_weights_only=False,
+        mode='auto',
+        period=10)
 
-    os.makedirs('/home/dlrc/Desktop/vae_models/logs/latent_dims_' + str(latent_dim), exist_ok=True)
-    tensorboard_callback = TensorBoard(log_dir='/home/dlrc/Desktop/vae_models/logs/latent_dims_' + str(latent_dim),
-    histogram_freq=0,
-    batch_size=batch_size,
-    write_graph=True,
-    write_grads=False,
-    write_images=True,
-    embeddings_freq=5,
-    embeddings_layer_names=["conv2d_transpose_3"],
-    embeddings_metadata=None)
+    os.makedirs('/data/logs/latent_dims_' + str(latent_dim), exist_ok=True)
+    tensorboard_callback = TensorBoard(
+        log_dir='/home/dlrc/Desktop/vae_models/logs/latent_dims_' + str(latent_dim),
+        histogram_freq=0,
+        batch_size=batch_size,
+        write_graph=True,
+        write_grads=False,
+        write_images=True,
+        embeddings_freq=5,
+        embeddings_layer_names=["conv2d_transpose_3"],
+        embeddings_metadata=None)
 
-    vae.fit_generator(blender_data_gen("/home/dlrc/Desktop/Img/train", batch_size), steps_per_epoch=7000, epochs=epochs,
-    callbacks=[checkpoint_callback,
-    tensorboard_callback
-    ],
-    # validation_data=blender_data_gen("/home/dlrc/Desktop/Img/validate", batch_size),
-    # validation_steps=4
+    vae.fit_generator(
+        blender_data_gen("/data/Img/train", batch_size),
+        steps_per_epoch=10,
+        epochs=epochs,
+        callbacks=[checkpoint_callback, tensorboard_callback],
+        # validation_data=blender_data_gen("/home/dlrc/Desktop/Img/validate", batch_size),
+        # validation_steps=4
     )
