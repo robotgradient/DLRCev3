a0994c32748ba1812dc5f1f3b58e915511420449
Marin Vlastelica <marin.vlastelicap@gmail.com>
2017-09-29 10:19:55 +0200

    Tweaked object detector to return bounding boxes

--- object_detection/nn_object_detection/object_detectors.py
+++ object_detection/nn_object_detection/object_detectors.py
@@ -4,28 +4,29 @@ from matplotlib import pyplot as plt
 import sys
 import tensorflow as tf
 sys.path.append("/home/dlrc/projects/tensorflow/models/object_detection")
-from utils import label_map_util
 import numpy as np
 
 
 # Path to frozen detection graph. This is the actual model that is used for the object detection.
-PATH_TO_CKPT = "/home/dlrc/projects/DLRCev3/object_detection/nn_object_detection/tf_train_dir/models/ssd_mobilenet_v1_lego/train/frozen_inference_graph.pb"
+PATH_TO_CKPT = "/home/dlrc/projects/DLRCev3/object_detection/nn_object_detection/tf_train_dir/models/faster_rcnn_resnet_lego_v1/train/frozen_inference_graph.pb"
 PATH_TO_LABELS = "/home/dlrc/projects/DLRCev3/object_detection/nn_object_detection/tf_train_dir/data/label_map.pbtxt"
 NUM_CLASSES = 2
-
-from utils import label_map_util
 NUM_CLASSES = 2
 
-label_map = label_map_util.load_labelmap(PATH_TO_LABELS)
-categories = label_map_util.convert_label_map_to_categories(label_map, max_num_classes=NUM_CLASSES, use_display_name=True)
-category_index = label_map_util.create_category_index(categories)
 
 
 def sorting_key(element1):
     bbox1=element1[0]
     return bbox1[2]/2
+img_res = (640,480) 
 
+def func(res):
+    box = res[0]
+    new_box = np.asarray([int(box[1]*img_res[0]), int(box[0]*img_res[1]), 
+                    int(box[3]*img_res[0]), int(box[2]*img_res[1])])
+    res = [new_box, res[1]]
 
+    return res
 
 class NNObjectDetector(object):
   
@@ -39,9 +40,10 @@ class NNObjectDetector(object):
         od_graph_def.ParseFromString(serialized_graph)
         tf.import_graph_def(od_graph_def, name='')
     self.detection_graph = detection_graph
-    label_map = label_map_util.load_labelmap(PATH_TO_LABELS)
-    self.categories = label_map_util.convert_label_map_to_categories(label_map, max_num_classes=NUM_CLASSES, use_display_name=True)
-    self.category_index = label_map_util.create_category_index(categories)
+    
+    #label_map = label_map_util.load_labelmap(PATH_TO_LABELS)
+    #self.categories = label_map_util.convert_label_map_to_categories(label_map, max_num_classes=NUM_CLASSES, use_display_name=True)
+    #self.category_index = label_map_util.create_category_index(categories)
     self.sess =  tf.Session(graph=self.detection_graph)
     
   def detect(self,image):
@@ -71,11 +73,13 @@ class NNObjectDetector(object):
 
 
 
-  def detect_with_threshold(self, image, threshold=0.95, return_closest=False):
+  def detect_with_threshold(self, image, threshold=0.95, return_closest=False, img_res = (640,480)):
 
     results = self.detect(image)
     results = list(zip(results[0], results[1]))
-    results = filter(lambda x: x[1] > threshold, results)    
+    results = filter(lambda x: x[1] > threshold, results)
+
+    results = map(func, results)
 
     print(results)
     if not return_closest:
--- object_detection/nn_object_detection/realtime_detection.py
+++ object_detection/nn_object_detection/realtime_detection.py
@@ -5,12 +5,12 @@ import numpy as np
 from matplotlib import pyplot as plt
 import sys
 import tensorflow as tf
-sys.path.append("/home/dlrc/projects/tensorflow/models/object_detection")
-from utils import label_map_util
+sys.path.append("/home/dlrc/projects/tensorflow/models/object_detection/utils")
+import label_map_util
 import numpy as np
 
 # Path to frozen detection graph. This is the actual model that is used for the object detection.
-PATH_TO_CKPT = "/home/dlrc/projects/DLRCev3/object_detection/nn_object_detection/tf_train_dir/models/ssd_mobilenet_v1_lego/train/frozen_inference_graph.pb"
+PATH_TO_CKPT = "/home/dlrc/projects/DLRCev3/object_detection/nn_object_detection/tf_train_dir/models/faster_rcnn_resnet_lego_v1/train/frozen_inference_graph.pb"
 PATH_TO_LABELS = "/home/dlrc/projects/DLRCev3/object_detection/nn_object_detection/tf_train_dir/data/label_map.pbtxt"
 NUM_CLASSES = 2
 
@@ -32,14 +32,16 @@ try:
         kernel = np.ones((5,5),np.float32)/25
         img = cv2.filter2D(img,-1,kernel)
         #image_np_expanded = np.expand_dims(img, axis=0)
-        res = detector.detect_with_threshold(img, threshold=0.9, return_closest=True)
+        res = detector.detect_with_threshold(img, threshold=0.9, return_closest=False)
         if res:
-            box, score = res
-            print("Main loop: ", box[0], len(box), score)
-            cv2.rectangle(img, (int(box[1]*img_res[0]), int(box[0]*img_res[1])), 
-                (int(box[3]*img_res[0]), int(box[2]*img_res[1])), (0, 255, 0), thickness=3)    
-            cv2.putText(img, str(score) + "%", (int(box[1]*img_res[0]),
-             int(box[0]*img_res[1])), cv2.FONT_HERSHEY_SIMPLEX, 0.6, color=(255,0,0))
+            for lego in res:
+                box, score = lego
+                print("Main loop: ", box[0], len(box), score)
+                # cv2.rectangle(img, (int(box[1]*img_res[0]), int(box[0]*img_res[1])), 
+                #     (int(box[3]*img_res[0]), int(box[2]*img_res[1])), (0, 255, 0), thickness=3)
+                cv2.rectangle(img, (box[0], box[1]), 
+                    (box[2], box[3]), (0, 255, 0), thickness=3)    
+                cv2.putText(img, str(score) + "%", (box[0], box[1]), cv2.FONT_HERSHEY_SIMPLEX, 0.6, color=(255,0,0))
 
         cv2.imshow("Camera", img)
         if cv2.waitKey(10) & 0xFF==27:
