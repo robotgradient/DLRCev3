605de0a4770ff4c2c34f3289349a9a74d05ba774
Ilia Kurenkov <ilia.kurenkov@gmail.com>
2017-09-30 11:50:14 +0200

    Fixed up vae training script and docker container

--- scripts/train_vae.py
+++ scripts/train_vae.py
@@ -14,51 +14,50 @@ from unsupervised_models.cvae import ConvolutionalVariationalAutoencoder
 
 IM_SIZE = 64
 
-if __name__ == '__main__':
+latent_dim = int(sys.argv[1])
+batch_size = 16
 
-    latent_dim = int(sys.argv[1])
-    batch_size = 16
+epochs = 1000
 
-    epochs = 1000
+import os
+from pathlib import Path
+PREFIX = Path.home()
+PREFIX = Path("/")
+# PREFIX = PREFIX / "dlrc"
 
-    vae = ConvolutionalVariationalAutoencoder(
-        image_dims=(IM_SIZE, IM_SIZE, 3), batch_size=batch_size, latent_dim=latent_dim, filters=32)
+vae = ConvolutionalVariationalAutoencoder(
+    image_dims=(IM_SIZE, IM_SIZE, 3), batch_size=batch_size, latent_dim=latent_dim, filters=32)
 
-    # vae.compile(optimizer='rmsprop', loss=vae.output_layers[0].losses[0])
-    vae.compile(
-        optimizer='rmsprop',
-        loss=None,
-        # metrics=[vae.output_layers[0].losses[0]]
-    )
-    print(vae.summary())
+vae.compile(
+    optimizer='rmsprop',
+    loss=None,)
+print(vae.summary())
 
-    os.makedirs('/data/weights/latent_dims_' + str(latent_dim), exist_ok=True)
-    checkpoint_callback = ModelCheckpoint(
-        "/data/weights/latent_dims_" + str(latent_dim) +
-        "/weights.{epoch:02d}.hdf5",
-        # monitor="custom_variational_layer_1/Mean_2:0",
-        verbose=1,
-        save_weights_only=False,
-        mode='auto',
-        period=10)
+weights_dir = PREFIX / ('weights/latent_dims_' + str(latent_dim))
+os.makedirs(weights_dir, exist_ok=True)
+checkpoint_callback = ModelCheckpoint(
+    str(weights_dir / ("latent_dims_" + str(latent_dim) + "/weights.{epoch:02d}.hdf5")),
+    verbose=1,
+    save_weights_only=False,
+    mode='auto',
+    period=10)
 
-    os.makedirs('/data/tensorboard-logdir/latent_dims_' + str(latent_dim), exist_ok=True)
-    tensorboard_callback = TensorBoard(
-        log_dir='/data/tensorboard-logdir/latent_dims_' + str(latent_dim),
-        histogram_freq=0,
-        batch_size=batch_size,
-        write_graph=True,
-        write_grads=False,
-        write_images=True,
-        embeddings_freq=5,
-        embeddings_layer_names=["conv2d_transpose_3"],
-        embeddings_metadata=None)
+tb_dir = PREFIX / ('tensorboard-logdir/latent_dims_' + str(latent_dim))
+os.makedirs(tb_dir, exist_ok=True)
+tensorboard_callback = TensorBoard(
+    log_dir=str(tb_dir),
+    histogram_freq=0,
+    batch_size=batch_size,
+    write_graph=True,
+    write_grads=False,
+    write_images=True,
+    embeddings_freq=5,
+    embeddings_layer_names=["conv2d_transpose_3"],
+    embeddings_metadata=None)
 
-    vae.fit_generator(
-        blender_data_gen("/data/Img/train", batch_size),
-        steps_per_epoch=3500,
-        epochs=epochs,
-        callbacks=[checkpoint_callback, tensorboard_callback],
-        # validation_data=blender_data_gen("/home/dlrc/Desktop/Img/validate", batch_size),
-        # validation_steps=4
-    )
+data_dir = PREFIX / "data"
+vae.fit_generator(
+    blender_data_gen(data_dir, batch_size),
+    steps_per_epoch=3500,
+    epochs=epochs,
+    callbacks=[checkpoint_callback, tensorboard_callback],)
