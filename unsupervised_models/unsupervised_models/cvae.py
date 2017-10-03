07b522f1fc51dd539582543e16ac1da42936b5b7
Ilia Kurenkov <ilia.kurenkov@gmail.com>
2017-10-03 23:48:02 +0200

    One more take on the reconstruction loss

--- unsupervised_models/unsupervised_models/cvae.py
+++ unsupervised_models/unsupervised_models/cvae.py
@@ -9,7 +9,6 @@ from keras.layers import Conv2D, Conv2DTranspose
 from keras.layers.normalization import BatchNormalization
 from keras.models import Model
 from keras import backend as K
-from keras import metrics
 
 # Custom loss layer
 
@@ -27,9 +26,11 @@ class CustomVariationalLayer(Layer):
     def vae_loss(self, x, x_decoded_mean_squash):
         x = K.flatten(x)
         x_decoded_mean_squash = K.flatten(x_decoded_mean_squash)
-        xent_loss = metrics.binary_crossentropy(x, x_decoded_mean_squash)
-        kl_loss = - 0.5 * K.mean(1 + self.z_log_var - K.square(self.z_mean) - K.exp(self.z_log_var), axis=-1)
-        loss =  K.mean(xent_loss + kl_loss)
+        xent_loss = K.mean(
+            K.binary_crossentropy(x, x_decoded_mean_squash, from_logits=True), axis=-1)
+        kl_loss = -0.5 * K.mean(
+            1 + self.z_log_var - K.square(self.z_mean) - K.exp(self.z_log_var), axis=-1)
+        loss = K.mean(xent_loss + kl_loss)
         return loss
 
     def call(self, inputs):
