d34dd87bf00c6849de6a29901c95f7eaf8012382
TheCamusean <judj.core@gmail.com>
2017-10-05 12:39:04 +0200

    kalman2

--- rick/rick/motion_control.py
+++ rick/rick/motion_control.py
@@ -471,6 +471,228 @@ def kalman_filter(odom_r,odom_l,pos_rob,marker_list, marker_map,Ts,P):
 	return pos_rob,P
 
 
+
+def kalman_filter2(odom_r,odom_l,pos_rob,marker_list, marker_map,Ts,P):
+
+	L = 14.5
+	R = 1.7
+
+	#From degrees to radians
+
+	odom_l = odom_l*pi/180
+	odom_r = odom_r*pi/180
+
+	# get increments
+
+	incr_r = R/2*(odom_r+odom_l)
+	incr_teta = R/L*(odom_l-odom_r) * 180/pi
+
+	## A and B matrixes
+	increment_R = R/2*(odom_r + odom_l)
+	increment_teta = R/L*(odom_l-odom_r) * 180/pi # We want the increment in teta in degrees
+
+	
+	A = np.identity(3)
+	A[0,2] = -increment_R*np.sin((pos_rob[2]+increment_teta/2)*pi/180)
+	A[1,2] =  increment_R*np.cos((pos_rob[2]+increment_teta/2)*pi/180)
+
+	c = np.cos((pos_rob[2]+increment_teta/2)*pi/180); s = np.sin((pos_rob[2]+increment_teta/2)*pi/180)
+
+	
+	B = np.zeros([3,2])
+
+	B[0,0] = R/2*c+R*increment_R*R/(2*L)*s
+	B[0,1] = R/2*c-R*increment_R*R/(2*L)*s
+
+	B[1,0] = R/2*s-increment_R*R/(2*L)*c
+	B[1,1] = R/2*s+increment_R*R/(2*L)*c
+
+	B[2,0] = -R/L
+	B[2,1] = R/L
+
+
+
+	# H Matrix
+	marker_list=np.array(marker_list)
+	markers = []
+	for i in range (0,marker_list.shape[0]):
+		#print("marker list",marker_list)
+		if marker_list[i,0] < 900:
+			distance = np.power(marker_map[i,0]-pos_rob[0],2) + np.power(marker_map[i,1]-pos_rob[1],2)
+		
+			if distance != 0:
+
+				markers.append(i)
+
+	#The size of the H array is related with the number of markers we see
+
+	H = np.zeros([len(markers)*3,3])
+	#H = np.zeros([len(markers)*2,3])
+
+	R = np.zeros([3*len(markers),3*len(markers)])
+	#R = np.zeros([2*len(markers),2*len(markers)])
+
+
+
+	for i in range(0,len(markers)):
+
+		distance = np.power(marker_map[markers[i],0]-pos_rob[0],2) + np.power(marker_map[markers[i],1]-pos_rob[1],2)
+		
+		H[i*3,0] = (marker_map[markers[i],1]-pos_rob[1])/distance
+		H[i*3,1] = -(marker_map[markers[i],0]-pos_rob[0])/distance
+		H[i*3,2] = -1
+
+		H[i*3+1,0] = (pos_rob[0]-marker_map[markers[i],0])/np.sqrt(distance)
+		H[i*3+1,1]= (pos_rob[1]-marker_map[markers[i],1])/np.sqrt(distance)
+		H[i*3+1,2] = 0
+
+		H[i*3+2,0] = 0
+		H[i*3+2,1] = 0
+		H[i*3+2,2] = -1
+		
+		'''H[i*2,0] = (marker_map[markers[i],1]-pos_rob[1])/distance
+		H[i*2,1] = -(marker_map[markers[i],0]-pos_rob[0])/distance
+		H[i*2,2] = -1
+
+		H[i*2+1,0] = (pos_rob[0]-marker_map[markers[i],0])/np.sqrt(distance)
+		H[i*2+1,1]= (pos_rob[1]-marker_map[markers[i],1])/np.sqrt(distance)
+		H[i*2+1,2] = 0
+		'''
+
+
+
+		#Noise of the measuremenets
+
+		R[i*3,i*3] = 1/np.power(10,5)
+		R[i*3+1,i*3+1] = 1/np.power(10,6)
+		R[i*3+2,i*3+2] = 1/np.power(10,6)
+
+
+		#R[i*2,i*2] = 1/np.power(10,5)
+		#R[i*2+1,i*2+1] = 1/np.power(10,6)
+
+
+
+	# Process noise
+	#print(H)
+
+	#noise variance of the encoders
+	noise_enc = 1/np.power(10,7)
+	var_noise_enc = np.power(noise_enc/Ts,2)
+	#noise variance of the model
+	Q = np.zeros([3,3])
+	Q[0,0] = 1/np.power(10,4)
+	Q[1,1] = 1/np.power(10,4)
+	Q[2,2] = 1/np.power(7.62,5)
+
+	# Kalman init
+
+	#Prediction step
+
+	P_pred = np.add(np.add(np.multiply(A,np.multiply(P,np.transpose(A))), var_noise_enc*np.dot(B,np.transpose(B))),Q)
+
+	pos_rob_pred = np.ones(3)
+
+	pos_rob_pred[0] = pos_rob[0] + incr_r*np.cos((pos_rob[2]+incr_teta/2)*pi/180)
+	pos_rob_pred[1] = pos_rob[1] + incr_r*np.sin((pos_rob[2]+incr_teta/2)*pi/180)
+	pos_rob_pred[2] = (pos_rob[2] + incr_teta)*pi/180
+
+	if pos_rob_pred[2] > pi:
+		pos_rob_pred[2] = pos_rob_pred[2]-(2*pi)
+	if pos_rob_pred[2] < -pi:
+		pos_rob_pred[2] = pos_rob_pred[2]+(2*pi)
+
+	#Measurements prediction & measurements
+
+
+	meas_vec = np.zeros(len(markers)*3)
+	#meas_vec = np.zeros(len(markers)*2)
+
+
+	z = np.zeros(3*len(markers))
+	#z = np.zeros(2*len(markers))
+
+	for i in range(0,len(markers)):
+
+		z[i*3] = np.arctan2(marker_map[markers[i],1]-pos_rob_pred[1],marker_map[markers[i],0]-pos_rob_pred[0])- pos_rob_pred[2]
+		z[i*3+1] = np.sqrt(np.power(marker_map[markers[i],0]-pos_rob_pred[0],2) + np.power(marker_map[markers[i],1]-pos_rob_pred[1],2))
+		z[i*3+2] = marker_map[markers[i],2]- pos_rob_pred[2]
+
+		#z[i*2] = np.arctan2(marker_map[markers[i],1]-pos_rob_pred[1],marker_map[markers[i],0]-pos_rob_pred[0])- pos_rob_pred[2]
+		#z[i*2+1] = np.sqrt(np.power(marker_map[markers[i],0]-pos_rob_pred[0],2) + np.power(marker_map[markers[i],1]-pos_rob_pred[1],2))
+		
+		
+		if z[i*3] > pi:
+			z[i*3] = z[i*3]-(2*pi)
+		if z[i*3] < -pi:
+			z[i*3] = z[i*3]+(2*pi)
+
+		if z[i*3+2] > pi:
+			z[i*3+2] = z[i*3+2]-(2*pi)
+		if z[i*3+2] < -pi:
+			z[i*3+2] = z[i*3+2]+(2*pi)
+		
+
+		'''
+		if z[i*2] > pi:
+			z[i*2] = z[i*2]-(2*pi)
+		if z[i*2] < -pi:
+			z[i*2] = z[i*2]+(2*pi)
+		'''
+
+
+			
+		meas_vec[i*3] = marker_list[markers[i],0]
+		meas_vec[i*3+1] = marker_list[markers[i],1]
+		meas_vec[i*3+2] = marker_list[markers[i],2]
+		'''
+		meas_vec[i*2] = marker_list[markers[i],0]
+		meas_vec[i*2+1] = marker_list[markers[i],1]
+		'''
+
+
+
+
+
+	HPHR = np.add(np.dot(H,np.dot(P_pred,np.transpose(H))),R)
+
+	K = np.dot(P_pred,np.dot(np.transpose(H),np.linalg.inv(HPHR)))
+
+
+
+	IKH = np.add(np.identity(3),-np.dot(K,H))
+
+	P = np.add(np.dot(IKH,np.dot(P_pred,np.transpose(IKH))),np.dot(K,np.dot(R,np.transpose(K))))
+
+	#Kalman's state estimation :
+
+
+
+	pos_incr = np.dot(K,np.add(z,-meas_vec))
+
+	print('expected: ',z)
+	print('real: ', meas_vec)
+
+
+	print('measurement error : ',pos_incr)
+
+	pos_rob = np.add(pos_rob_pred,-pos_incr)
+
+
+
+
+
+	pos_rob[2] = pos_rob[2]* 180/pi
+	if pos_rob[2] >360:
+		pos_rob[2] = pos_rob[2] - 360
+	elif pos_rob[2] < 0 :
+		pos_rob[2] = 360 + pos_rob[2]
+
+	#print(new_pos_rob)
+	return pos_rob,P
+
+
+
 def odom_estimation(odom_r,odom_l,pos_rob):
 
 
